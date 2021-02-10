82893dd1c77ebad945a623a558d7c0f6324499d2
benmhansford <benmhansford@gmail.com>
2021-02-10 19:05:49 +0000

    Copied code from U4A4

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const btn = document.querySelector('button');
-
-function random(number) {
-  return Math.floor(Math.random() * (number+1));
-}
-
-btn.onclick = function() {
-  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + 255 + ')';
-  document.body.style.backgroundColor = rndCol;
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+ 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
 }
